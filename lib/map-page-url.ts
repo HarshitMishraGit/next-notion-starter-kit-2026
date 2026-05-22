755e30d64708c279ea023b0bc61d18ca68c03548
Harshit Mishra <harshitlove28@outlook.com>
2026-05-22 18:49:11 +0530

    trying to update the weburls in production in sluggify manner

--- lib/map-page-url.ts
+++ lib/map-page-url.ts
@@ -1,13 +1,14 @@
 import { type ExtendedRecordMap } from 'notion-types'
 import { parsePageId, uuidToId } from 'notion-utils'
 
-import { includeNotionIdInUrls } from './config'
+// import { includeNotionIdInUrls } from './config'
 import { getCanonicalPageId } from './get-canonical-page-id'
 import { type Site } from './types'
 
 // include UUIDs in page URLs during local development but not in production
 // (they're nice for debugging and speed up local dev)
-const uuid = !!includeNotionIdInUrls
+// default const uuid = !!includeNotionIdInUrls
+const uuid = true // so that pages with same title have different URLs
 
 export const mapPageUrl =
   (site: Site, recordMap: ExtendedRecordMap, searchParams: URLSearchParams) =>
